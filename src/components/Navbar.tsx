ccf03bf748ce4786df82cfd2f87182e9c647ce38
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-03-29 21:22:57 +0800

    added working navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,6 +11,13 @@ import {
     NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardTitle,
+} from "@/components/ui/card"
+
 export default function Navbar() {
     return (
         <div className="w-screen flex px-10 py-5">
@@ -18,7 +25,24 @@ export default function Navbar() {
                 <NavigationMenu>
                     <NavigationMenuList>
                         <NavigationMenuItem>
-                            <Link to={"/"}>Test</Link>
+                            <NavigationMenuTrigger>
+                                <Link to={"/"}>Home</Link>
+                            </NavigationMenuTrigger>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Crowds</NavigationMenuTrigger>
+                            <NavigationMenuContent className="w-200">
+                                <ul className="gap-2">
+                                    <Card>
+                                        <Link to={"/crowd/create"}>
+                                            <CardContent>
+                                                <CardTitle>Create Crowds</CardTitle>
+                                                <CardDescription>Setup and Configure your Virtual Crowds for your surveys!</CardDescription>
+                                            </CardContent>
+                                        </Link>
+                                    </Card>
+                                </ul>
+                            </NavigationMenuContent>
                         </NavigationMenuItem>
                     </NavigationMenuList>
                 </NavigationMenu>
