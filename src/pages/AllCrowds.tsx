53f653fd50ee48a74c01348799dddb6e41f64adc
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-03-31 00:33:50 +0800

    updated page title styling

--- src/pages/AllCrowds.tsx
+++ src/pages/AllCrowds.tsx
@@ -1,7 +1,7 @@
 export default function AllCrowds() {
     return (
         <div className="w-full h-full">
-            <h1>All Crowds</h1>
+            <h1 className="text-3xl font-bold">All Crowds</h1>
         </div>
     )
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
     return (
         <div className="w-full h-full">
-            <h1>Dashboard</h1>
+            <h1 className="text-3xl font-bold">Dashboard</h1>
         </div>
     )
 }
