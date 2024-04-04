2a412f037be38171efd9cb1f6b9ad48615e08f8b
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-04-05 00:53:54 +0800

    updated EducationCard

--- src/components/BioCards.tsx
+++ src/components/BioCards.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, FC } from "react";
+import { FC } from "react";
 
 import {
     Card,
@@ -9,7 +9,6 @@ import {
 } from "@/components/ui/card";
 
 import {
-    Form,
     FormControl,
     FormDescription,
     FormField,
@@ -26,13 +25,11 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Control, useForm } from "react-hook-form";
+import { Control } from "react-hook-form";
 import { z } from "zod";
 
 export const builderSchema = z.object({
@@ -41,6 +38,8 @@ export const builderSchema = z.object({
         value: z.object({
             mainValue: z.string(),
             subValue: z.string().optional(),
+            rangeFromValue: z.string().optional(),
+            rangeToValue: z.string().optional(),
             groupValue: z.array(z.string()),
             type: z.string().optional(),
             specify: z.boolean().default(false).optional(),
@@ -113,7 +112,7 @@ export function AgeCard(props: { index: number, control: Control<z.infer<typeof 
                                 : <div className="w-full flex items-center gap-2">
                                     <FormField
                                         control={props.control}
-                                        name={`data.${props.index}.value.mainValue`}
+                                        name={`data.${props.index}.value.rangeFromValue`}
                                         render={({ field }) => (
                                             <div className="w-full">
                                                 <FormLabel className="text-large font-semibold">From</FormLabel>
@@ -125,7 +124,7 @@ export function AgeCard(props: { index: number, control: Control<z.infer<typeof 
                                     />
                                     <FormField
                                         control={props.control}
-                                        name={`data.${props.index}.value.subValue`}
+                                        name={`data.${props.index}.value.rangeToValue`}
                                         render={({ field }) => (
                                             <div className="w-full">
                                                 <FormLabel className="text-large font-semibold">To</FormLabel>
@@ -252,48 +251,8 @@ export function GenderCard(props: { index: number, control: Control<z.infer<type
     )
 }
 
-export function EducationCard(props: { trigger: number, passData: (data: string) => void }) {
-    const formSchema = z.object({
-        course: z.array(z.string()),
-        years: z.object({
-            total: z.number(),
-            specifyRange: z.boolean(),
-            rangeFrom: z.number(),
-            rangeTo: z.number()
-        })
-    });
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            course: [""],
-            years: {
-                total: 4,
-                specifyRange: false,
-                rangeFrom: 1,
-                rangeTo: 4
-            }
-        },
-    });
-
-    // temporary solution to global saving
-    const refSubmitButton = useRef<HTMLButtonElement>(null);
-
-    const onSubmit = (data: z.infer<typeof formSchema>) => {
-        const temp = {
-            course: data.course,
-            totalYears: data.years.total,
-            includedYears: data.years.specifyRange === false ? "All" : `${data.years.rangeFrom} to ${data.years.rangeTo}`
-        }
-
-        props.passData(JSON.stringify(temp));
-    }
-
-    useEffect(() => {
-        if (props.trigger) {
-            refSubmitButton?.current?.click();
-        }
-    }, [props.trigger])
+export function EducationCard(props: { index: number, control: Control<z.infer<typeof builderSchema>>, data: object }) {
+    const data = JSON.parse(JSON.stringify(props.data));
 
     return (
         <Card className="w-full">
@@ -301,91 +260,86 @@ export function EducationCard(props: { trigger: number, passData: (data: string)
                 <CardTitle>Education</CardTitle>
                 <CardDescription>Define your crowds' education details</CardDescription>
             </CardHeader>
-            <CardContent>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
-                        <FormField
-                            control={form.control}
-                            name="course"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-large font-semibold">Course</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type="text"
-                                            value={field.value}
-                                            onChange={field.onChange}
-                                            placeholder="Enter course names, comma separated if multiple"
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="years.total"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-large font-semibold">Total Years</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type="number"
-                                            value={field.value}
-                                            onChange={field.onChange}
-                                            placeholder="Enter average amount of years to complete the courses"
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="years.specifyRange"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <div className="flex items-center gap-2">
-                                        <FormControl>
-                                            <Checkbox
-                                                checked={field.value}
-                                                onCheckedChange={field.onChange}
-                                            />
-                                        </FormControl>
-                                        <FormLabel>Specify Year Range? <span className="text-gray-400">(leave unchecked if all are included)</span></FormLabel>
-                                    </div>
-                                </FormItem>
-                            )}
-                        />
-                        {form.getValues("years.specifyRange") === true ?
-                            <div className="w-full flex items-center gap-2">
-                                <FormField
-                                    control={form.control}
-                                    name="years.rangeFrom"
-                                    render={({ field }) => (
-                                        <div className="w-full">
-                                            <FormLabel>From</FormLabel>
-                                            <FormControl>
-                                                <Input type="number" value={field.value} onChange={field.onChange} placeholder="1" />
-                                            </FormControl>
-                                        </div>
-                                    )}
+            <CardContent className="flex flex-col gap-2">
+                <FormField
+                    control={props.control}
+                    name={`data.${props.index}.value.mainValue`}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="text-large font-semibold">Course</FormLabel>
+                            <FormControl>
+                                <Input
+                                    type="text"
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                    placeholder="Enter course names, comma separated if multiple"
                                 />
-                                <FormField
-                                    control={form.control}
-                                    name="years.rangeTo"
-                                    render={({ field }) => (
-                                        <div className="w-full">
-                                            <FormLabel>To</FormLabel>
-                                            <FormControl>
-                                                <Input type="number" value={field.value} onChange={field.onChange} placeholder="4" />
-                                            </FormControl>
-                                        </div>
-                                    )}
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={props.control}
+                    name={`data.${props.index}.value.subValue`}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="text-large font-semibold">Total Years</FormLabel>
+                            <FormControl>
+                                <Input
+                                    type="number"
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                    placeholder="Enter average amount of years to complete the courses"
                                 />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={props.control}
+                    name={`data.${props.index}.value.specify`}
+                    render={({ field }) => (
+                        <FormItem>
+                            <div className="flex items-center gap-2">
+                                <FormControl>
+                                    <Checkbox
+                                        checked={field.value}
+                                        onCheckedChange={field.onChange}
+                                    />
+                                </FormControl>
+                                <FormLabel>Specify Year Range? <span className="text-gray-400">(leave unchecked if all are included)</span></FormLabel>
                             </div>
-                            : <></>}
-                        <Button className="hidden" ref={refSubmitButton} type="submit"></Button>
-                    </form>
-                </Form>
+                        </FormItem>
+                    )}
+                />
+                {data.specify === true ?
+                    <div className="w-full flex items-center gap-2">
+                        <FormField
+                            control={props.control}
+                            name={`data.${props.index}.value.rangeFromValue`}
+                            render={({ field }) => (
+                                <div className="w-full">
+                                    <FormLabel>From</FormLabel>
+                                    <FormControl>
+                                        <Input type="number" value={field.value} onChange={field.onChange} placeholder="1" />
+                                    </FormControl>
+                                </div>
+                            )}
+                        />
+                        <FormField
+                            control={props.control}
+                            name={`data.${props.index}.value.rangeToValue`}
+                            render={({ field }) => (
+                                <div className="w-full">
+                                    <FormLabel>To</FormLabel>
+                                    <FormControl>
+                                        <Input type="number" value={field.value} onChange={field.onChange} placeholder="4" />
+                                    </FormControl>
+                                </div>
+                            )}
+                        />
+                    </div>
+                    : <></>}
             </CardContent>
         </Card>
     )
